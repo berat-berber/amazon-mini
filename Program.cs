d422b044f5869131f0d463eeaf741a8e1317061c
berat-berber <berat0berber@gmail.com>
2026-08-11 16:02:12 +0300

    Add scalar openapi documentation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using amazonmini;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
