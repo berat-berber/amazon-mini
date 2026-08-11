f8237838a89b582f603478f9757bc1494a76a331
berat-berber <berat0berber@gmail.com>
2026-08-11 18:20:05 +0300

    Update namespace

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace MyApp.Namespace
+namespace amazonmini.Namespace
 {
     [Route("api/[controller]")]
     [ApiController]
